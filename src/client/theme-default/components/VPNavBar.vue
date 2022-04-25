bfcc6bef518ebbb7d16eb219a4f903232107215b
Kia Ishii <kia.king.08@gmail.com>
2022-04-25 19:37:27 +0900

    wip: working on

---
+++ src/client/theme-default/components/VPNavBar.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import VPNavBarTitle from './VPNavBarTitle.vue'
+// import VPNavBarSearch from './VPNavBarSearch.vue'
+// import VPNavBarMenu from './VPNavBarMenu.vue'
+// import VPNavBarAppearance from './VPNavBarAppearance.vue'
+// import VPNavBarSocialLinks from './VPNavBarSocialLinks.vue'
+// import VPNavBarExtra from './VPNavBarExtra.vue'
+import VPNavBarHamburger from './VPNavBarHamburger.vue'
+
+defineProps<{
+  isScreenOpen: boolean
+}>()
+
+defineEmits<{
+  (e: 'toggle-screen'): void
+}>()
+</script>
+
+<template>
+  <div class="VPNavBar">
+    <div class="container">
+      <VPNavBarTitle />
+
+      <div class="content">
+        <!-- <VPNavBarSearch class="search" /> -->
+        <!-- <VPNavBarMenu class="menu" /> -->
+        <!-- <VPNavBarAppearance class="appearance" /> -->
+        <!-- <VPNavBarSocialLinks class="social-links" /> -->
+        <!-- <VPNavBarExtra class="extra" /> -->
+        <VPNavBarHamburger
+          class="hamburger"
+          :active="isScreenOpen"
+          @click="$emit('toggle-screen')"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.VPNavBar {
+  position: relative;
+  border-bottom: 1px solid var(--vp-c-divider-light);
+  height: var(--vp-nav-height-mobile);
+  background-color: var(--vt-c-bg);
+  transition: border-color 0.5s, background-color 0.5s;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: var(--vp-layout-max-width);
+}
+
+.content {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-grow: 1;
+}
+
+.menu + .appearance {
+  margin-left: 8px;
+}
+
+.menu + .social-links {
+  margin-left: 12px;
+}
+
+.appearance + .social-links {
+  margin-left: 12px;
+}
+</style>
---
+++ src/client/theme-default/components/VPNavBarTitle.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { site } = useData()
+</script>
+
+<template>
+  <a class="VPNavBarTitle" href="/">
+    {{ site.title }}
+  </a>
+</template>
+
+<style scoped>
+.VPNavBarTitle {
+  display: flex;
+  align-items: center;
+  padding: 0 24px;
+  height: var(--vp-nav-height-mobile);
+  font-size: 16px;
+  font-weight: 600;
+  color: var(--vp-c-text-1);
+  transition: opacity 0.25s;
+}
+
+.VPNavBarTitle:hover {
+  opacity: 0.6;
+}
+</style>
